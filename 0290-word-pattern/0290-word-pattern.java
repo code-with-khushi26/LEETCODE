f027b386c12f7f154e578e5bf44661d8feb997c3
Khushi Gupta <itzkhushi2611@gmail.com>
2026-09-03 14:06:51 +0530

    Time: 1 ms (83.96%), Space: 42.8 MB (55.88%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        HashMap map1=new HashMap<>();
+        HashMap map2=new HashMap<>();
+
+        
+        String st[]=s.split(" ");
+
+        if(pattern.length()!=st.length){
+            return false;
+        }
+        
+        for(int i=0;i<pattern.length();i++){
+            char a=pattern.charAt(i);
+            String b=st[i];
+
+            if(map1.containsKey(a) && !map1.get(a).equals(b)){
+                return false;
+            }
+
+            if(map2.containsKey(b) && !map2.get(b).equals(a)){
+                return false;
+            }
+
+            map1.put(a,b);
+            map2.put(b,a);
+        }
+        
+
+        return true;
+    }
+}
